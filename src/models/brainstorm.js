19aeebcec50b3dbcd0f899b9946857fba37ada47
David Cameron <david.l.cameron@gmail.com>
2012-12-13 17:10:14 -0500

    Inheriting to 'inherited' subdocument, added fieldsFor function to send JSON to the CMS GUI to create input fields

--- src/models/brainstorm.js
+++ src/models/brainstorm.js
@@ -15,13 +15,13 @@ var inputMap =
 };
 
 
+// Promise wrapper for Mongo find
 function find(record) {
     if (typeof record.name === 'undefined') {
         recordName = record;
     } else {
         recordName = record.name;
     }
-    //console.log("name:", recordName);
     var defer = Q.defer();
     
     (function (defer, recordName) {
@@ -39,28 +39,38 @@ function find(record) {
     return defer.promise;
 }
 
-function validate (record, defer) {
+
+// Inherits defaults and structure up the chain recursively
+// Adds inherited structure in inherited : {structure: {}}
+function populate (record, defer) {
+    // Setup in the first populate call
     if (typeof defer === 'undefined') {
         defer = Q.defer();
+        record.inherited = {};
+        record.inherited.structure = {};
+        record.inherited.data = {};
+        record.inherited.archetype = record.archetype;
+        if(typeof record.structure === 'undefined') {
+            record.structure = {};
+        }
     }
-    console.log(record);
-    if (typeof record.archetype === 'undefined') {
-        console.log('isRoot');
+
+    if (typeof record.inherited.archetype === 'undefined') {
+        // Inheritance chain ends when there's no archetype
         defer.resolve(record);
     } else {
+        // Closure to send deferred and record to inherit
         (function (defer, record) {
-
-            return find(record.archetype).then(function (newRecord) {
-
+            return find(record.inherited.archetype).then(function (newRecord) {
                 record = inherit(record, newRecord);
-
+                // No record w/ the name of the archetype
                 if (!record) {
                     defer.reject('Faild Inherit');
                     return;
                 }
-
                 return (function (defer) {
-                    validate (record, defer);
+                    // recurse
+                    populate (record, defer);
                 })(defer);
             });
         })(defer, record);
@@ -71,21 +81,25 @@ function validate (record, defer) {
 
 function inherit (record, parent) {
     for (var x in parent.structure) {
-        if(parent.structure[x].required && typeof record.data[x] === 'undefined') {
+        if (parent.structure[x].required && typeof record.data[x] === 'undefined') {
             return false;
         }
-        if(parent.structure[x].defaultValue && typeof record.data[x] === 'undefined') {
-            record.data[x] = parent.structure[x].defaultValue;
+        if (parent.structure[x].defaultValue && typeof record.data[x] === 'undefined') {
+            record.inherited.data[x] = parent.structure[x].defaultValue;
         }
+        if (typeof record.structure[x] === 'undefined') {
+            record.inherited.structure[x] = parent.structure[x];
+        }
+
     }
 
     for (var y in parent.data) {
         if(typeof record.data[y] === 'undefined') {
-            record.data[y] = parent.data[y];
+            record.inherited.data[y] = parent.data[y];
         }
     }
 
-    record.archetype = parent.archetype;
+    record.inherited.archetype = parent.archetype;
 
     return record;
 }
@@ -104,10 +118,10 @@ function insert (record) {
 
 var validPost =
 {
-    name: 'hello-world',
+    name: 'new-test-post',
     archetype: 'post',
     data: {
-        title: 'Hello World',
+        title: 'Hello World2',
         body: '<p>Hello, this is my first post!</p>'
     }
 };
@@ -124,17 +138,48 @@ function create (record) {
         return "OK";
     })
     .then(function () {
-        return validate(record);
+        return populate(record);
     })
-    .then(insert)
+    .then(function (r) {
+        console.log(r);
+    })
+    //.then(insert)
     .fail(function (error) {
         console.log('fail', error);
     });
 }
 
-function fieldsFor (record) {
-    find(record);
-    //.then()
+function createFields (record) {
+    console.log(record);
+    fieldJson = {};
+
+    for (var y in record.inherited.structure) {
+        fieldJson[y] = inputMap[record.inherited.structure[y].type];
+    }
+
+    for (var x in record.structure) {
+        fieldJson[x] = inputMap[record.structure[x].type];
+    }
+    return JSON.stringify(fieldJson);
 }
 
-create(validPost);
+function fieldsFor (record) {
+    var defer = Q.defer();
+
+    populate(record)
+    .then(createFields)
+    .then(function (json) {
+        defer.resolve(json);
+    })
+    .fail(function (err) {
+        defer.reject(err);
+    });
+
+    return defer.promise;
+}
+
+find('post')
+.then(fieldsFor)
+.then(function (r) {
+    console.log(r);
+});
